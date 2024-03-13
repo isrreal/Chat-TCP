2cdb21a72f208c15dfdc7938c8a1aa80f29edd32
leggen-assis <souzaferreira437@gmail.com>
2024-03-13 08:56:47 -0300

    comments added for best understanding

--- ChatTCP/client.cpp
+++ ChatTCP/client.cpp
@@ -7,11 +7,15 @@
 
 #define PORT 60000
 
+// Function to send messages to the server
 void sendMessages(int sock) {
     std::string message;
+    // Loop to continuously send messages while the program is running
     while (true) {
+        // Wait for user input to send a message
         std::getline(std::cin, message);
-        send(sock, message.c_str(), message.length(), 0);
+        // Send the message to the server
+        send(sock, message.data(), message.length(), 0);
     }
 }
 
@@ -19,37 +23,45 @@ int main(int argc, char const* argv[]) {
     std::string mensagem = "";
     int sock = 0; 
     int reader = 1;
-    struct sockaddr_in enderecoDoServer;
+    struct sockaddr_in serverAddress;
     char buffer[1024] = {0};
 
+    // Create a socket
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        std::cout << "Erro na criação do socket!" << std::endl;
+        std::cout << "Error creating socket!" << std::endl;
         return -1;
     }
 
-    enderecoDoServer.sin_family = AF_INET;
-    enderecoDoServer.sin_port = htons(PORT);
+    // Configure the server address
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(PORT);
 
-    // Convertendo endereços IPv4 e IPv6 de texto para sua forma binária
-
-    if(inet_pton(AF_INET, "127.0.0.1", &enderecoDoServer.sin_addr) <= 0) {
-        std::cout << "Endereço inválido! Este IP não é suportado!" << std::endl;
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, "127.0.0.1", &serverAddress.sin_addr) <= 0) {
+        std::cout << "Invalid address! This IP is not supported!" << std::endl;
         return -1;
     }
 
-    if (connect(sock, (struct sockaddr*) &enderecoDoServer, sizeof(enderecoDoServer)) < 0) {
-        std::cout << "Falha de conexão: Não foi possível estabelecer uma conexão sobre este socket!" << std::endl;
+    // Connect the client to the server
+    if (connect(sock, (struct sockaddr*) &serverAddress, sizeof(serverAddress)) < 0) {
+        std::cout << "Connection failed: Could not establish a connection on this socket!" << std::endl;
         return -1;
     }
     
+    // Start a thread to send messages to the server
     std::thread senderThread(sendMessages, sock);
 
+    // Loop to receive messages from the server
     while (true) {
+        // Clear the buffer
         memset(buffer, 0, sizeof(buffer));
+        // Read data received from the server
         reader = read(sock, buffer, 1024);
+        // Display the received message from the server
         std::cout << buffer << std::endl;
     }
 
+    // Wait for the message sending thread to finish
     senderThread.join();
 
     return 0;
--- ChatTCP/server.cpp
+++ ChatTCP/server.cpp
@@ -7,10 +7,14 @@
 
 #define PORT 60000
 
+// Function that sends messages to the client
 void sendMessages(int sock) {
     std::string message;
+    // Infinite loop to send messages while the program is running
     while (true) {
+        // Wait for user input to send a message
         std::getline(std::cin, message);
+        // Send the message to the client
         send(sock, message.c_str(), message.length(), 0);
     }
 }
@@ -24,47 +28,61 @@ int main(int argc, char const* argv[]) {
     int address_length = sizeof(address);
     char buffer[1024] = {0};
 
+    // Create the socket
     obj_server = socket(AF_INET, SOCK_STREAM, 0);
 
+    // Check if the socket was created successfully
     if (obj_server == 0) {
         perror("Opening of Socket Failed !");
         exit(EXIT_FAILURE);
     }
 
+    // Configure socket options, such as address reuse
     if (setsockopt(obj_server, SOL_SOCKET, SO_REUSEADDR, &opted, sizeof(opted))) {
         perror("Can't set the socket");
         exit(EXIT_FAILURE);
     }
 
+    // Configure the server address
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
+    // Bind the IP address to the port
     if (bind(obj_server, (struct sockaddr*)&address, sizeof(address)) < 0) {
         perror("Binding of socket failed !");
         exit(EXIT_FAILURE);
     }
 
+    // Enable the server to accept connections, with a queue of up to 3 pending connections
     if (listen(obj_server, 3) < 0) {
         perror("Can't listen from the server !");
         exit(EXIT_FAILURE);
     }
 
+    // Wait for a connection from a client
     if ((sock = accept(obj_server, (struct sockaddr*)&address, (socklen_t*)&address_length)) < 0) {
         perror("Accept");
         exit(EXIT_FAILURE);
     }
 
-    std::cout << "Conexão estabelecida com " << address.sin_addr.s_addr << "!" << std::endl;
+    // Inform that a connection was established and show the client's address
+    std::cout << "Connection established with " << address.sin_addr.s_addr << "!" << std::endl;
 
+    // Create a thread to send messages to the client
     std::thread senderThread(sendMessages, sock);
 
+    // Loop to receive messages from the client
     while (true) {
+        // Clear the buffer
         memset(buffer, 0, sizeof(buffer));
+        // Read the received data from the client
         reader = read(sock, buffer, 1024);
+        // Display the received message from the client
         std::cout << buffer << std::endl;
     }
 
+    // Wait for the message sending thread to finish
     senderThread.join();
 
     return 0;
